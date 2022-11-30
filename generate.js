3ab5f812573516bd6bd9cb06b411a395290166e9
William Rausch <nolanrausch@gmail.com>
2022-11-30 07:13:12 -0500

    gen

--- generate.js
+++ generate.js
@@ -1,18 +1,46 @@
 let gen = document.querySelector('#gen');
 
 
-gen.addEventListener('click', generate); 
+gen.addEventListener('click', slow); 
+gen2.addEventListener('click', normal); 
+gen3.addEventListener('click', hype); 
+
 music_vae = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2');
 music_vae.initialize();
+norm = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/groovae_2bar_humanize');
+norm.initialize();
+hype = new mm.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/multitrack_fb256');
+hype.initialize();
 
-// Create a player to play the sampled sequence.
 vaePlayer = new mm.Player();
 vae_temperature = 1.5;
 
+function hype(){
+ 
+  console.log("hype");
+  if (vaePlayer.isPlaying()) {
+    vaePlayer.stop();
+    return;
+  }
+  hype
+  .sample(1, 1.5)
+  .then((sample) => vaePlayer.start(sample[0]));
+}
+function normal(){
+ 
+  console.log("norm");
+  if (vaePlayer.isPlaying()) {
+    vaePlayer.stop();
+    return;
+  }
+  norm
+  .sample(1, vae_temperature)
+  .then((sample) => vaePlayer.start(sample[0]));
 
-
-function generate(){
-  console.log("Generatign music");
+}
+function slow(){
+ 
+  console.log("slow");
   if (vaePlayer.isPlaying()) {
     vaePlayer.stop();
     return;
--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 let input = document.querySelector('.userInput');
 let button = document.querySelector('.button');
 let display= document.querySelector('.searchResults');
-
-
 let happy = document.querySelector('#H');
 let mad = document.querySelector('#M');
 let sad = document.querySelector('#S');
-
 let fear = document.querySelector('#F');
 let disgust = document.querySelector('#D');
 let surprise = document.querySelector('#SU');
@@ -95,82 +92,3 @@ let song = Math.floor(Math.random() * num);
       });
 
     });
-    /*
-    function playSong(x) {
-console.log('ID LOOK' + x);
-      let index = Number(x);
-      let player = document.getElementById('music-player');
-      let musicPreview = data.results[index].previewUrl;
-      player.setAttribute('src', musicPreview);
-      player.play();
-    }
-*/
-
-}
-/*
-function Search () {
-  userInput = input.value;
-  
-  event.preventDefault();
-
- // query = userInput.split(" ").join("+");
- console.log(query);
-
-  let url = ("https://itunes.apple.com/search?term=" + query)
-
-    fetch(url)
-        .then(function(response){
-
-          response.json().then(function(data){
-          console.log(data);
-
-         
-
-           for (var i = 0; i < 20; i++) { 
-
-             let results = document.createElement('div');
-              results.setAttribute('class', 'result');
-              results.id = i;
-              console.log(results.id);
-
-
-
-             results.innerHTML += 
-             `
-               <div class="songContainer">
-                 <img src="${data.results[i].artworkUrl100}">
-               
-               <p class="songName">
-                 ${data.results[i].trackName}
-               </p>
-               <p class="artistName">
-                 ${data.results[i].artistName}
-                 </p>
-             `
-             results.addEventListener('click', function(event){
-                playSong(this.id);
-                console.log(this.id); 
-              });
-
-               display.appendChild(results);
-               function playSong(x) {
-
-                 let index = Number(x);
-                 let player = document.getElementById('music-player');
-                 let musicPreview = data.results[index].previewUrl;
-                 player.setAttribute('src', musicPreview);
-                 player.play();
-               }
-
-           }
-
-
-
-         
-
-      });
-
-    });
-
-}
-*/
